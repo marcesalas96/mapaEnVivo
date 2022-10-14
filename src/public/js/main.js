a43b71088ac7a40c53774d039fea2acef7d74c7a
marcesalas96 <marcesalas1996@gmail.com>
2022-10-14 18:34:37 -0300

    Bug fixed

--- src/public/js/main.js
+++ src/public/js/main.js
@@ -1,19 +1,29 @@
 const socket = io()
-const map = L.map('map-container').setView([-24.793583, -65.419647],13.5)
+const map = L.map('map-container').setView([-24.793583, -65.419647], 12.8)
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
-map.locate({watch: true, enableHighAccuracy:true})
-map.on('locationfound', (e) => {
-    const cords = [e.latitude, e.longitude]
-    socket.emit("clientCoords", cords)
-})
+
+
+setInterval(() => {
+    navigator.geolocation.getCurrentPosition(e => {
+        const { latitude, longitude } = e.coords
+        const cords = [latitude, longitude]
+        socket.emit("clientCoords", cords);
+    })
+}, 5000)
+
+
+let marker
 socket.on("serverCoords", async (coords) => {
-    const marker = L.marker(coords)
-    map.removeLayer(marker)
+    if (marker) {
+        console.log("Entre if")
+        map.removeLayer(marker)
+    }
+    marker = L.marker(coords)
     marker.bindPopup("Estas aqui!")
     map.addLayer(marker);
-    })
-    
+})
+
 
--- src/sockets/socket.js
+++ src/sockets/socket.js
@@ -1,7 +1,9 @@
+let contador = 0
 module.exports = socketIo = (io) => {
     io.on('connection', (socket) => {
         console.log("New socket conected, id: ", socket.id)
         socket.on("clientCoords", cords => {
+            console.log(cords)
             io.emit("serverCoords", cords)
         })
     })
